9bdfc0a99b3a34c636ad49c0388e985e71ef1a34
ahmedraafat34 <ahmedraafat@weaccept.co>
2020-06-11 13:05:32 +0200

    "Jawwal- pay Edits"

--- androidsdk/src/main/java/com/example/androidsdk/Pay.java
+++ androidsdk/src/main/java/com/example/androidsdk/Pay.java
@@ -2,8 +2,13 @@ package com.example.androidsdk;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,14 +19,88 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.net.URI;
 import java.net.URL;
 
-public class Pay  extends AppCompatActivity {
+public class Pay extends AppCompatActivity {
     public String PaymentKey;
     public int IframeID;
+    public String URL = "";
+    public String Endpoint = "";
+    String success = "";
+    String Id = "";
+    String amount_cents = "";
+    String integration_id = "";
+    String has_parent_transaction = "";
+    String txn_response_code = "";
+    String acq_response_code = "";
+
+    final Intent result = new Intent();
+
     public void StartPayment(String paymentKey, int iframeID) {
 
-        WebView mywebview = (WebView) findViewById(R.id.webView);
+        final WebView mywebview = (WebView) findViewById(R.id.webView);
+
+        mywebview.setWebViewClient(new WebViewClient() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (view.getOriginalUrl().contains(Endpoint)) {
+
+                    URL = view.getOriginalUrl();
+                    Uri uri = Uri.parse(URL);
+
+                    if (uri != null) {
+                        success = uri.getQueryParameter("success");
+                        Id = uri.getQueryParameter("id");
+                        amount_cents = uri.getQueryParameter("amount_cents");
+                        integration_id = uri.getQueryParameter("integration_id");
+                        has_parent_transaction = uri.getQueryParameter("has_parent_transaction");
+                        txn_response_code = uri.getQueryParameter("txn_response_code");
+                        acq_response_code = uri.getQueryParameter("acq_response_code");
+
+
+                        mywebview.destroy();
+
+                    }
+
+
+                    result.putExtra("success", success);
+                    result.putExtra("ID", Id);
+                    result.putExtra("amount_cents", amount_cents);
+                    result.putExtra("integration_id", integration_id);
+                    result.putExtra("has_parent_transaction", has_parent_transaction);
+                    result.putExtra("txn_response_code", txn_response_code);
+                    result.putExtra("acq_response_code", acq_response_code);
+
+                    setResult(RESULT_OK, result);
+
+                    finish();
+
+
+                }
+
+            }
+
+        });
+
         mywebview.getSettings().setJavaScriptEnabled(true);
-        mywebview.setWebViewClient(new WebViewClient());
         mywebview.getSettings().setLoadWithOverviewMode(true);
         mywebview.getSettings().setUseWideViewPort(true);
         mywebview.getSettings().setSupportZoom(true);
@@ -38,8 +117,9 @@ public class Pay  extends AppCompatActivity {
         mywebview.loadUrl("https://accept.paymobsolutions.com/api/acceptance/iframes/" + IframeID + "?payment_token=" + PaymentKey);
 
     }
+
     @Override
-    public void onConfigurationChanged(Configuration newConfig){
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
 
@@ -47,12 +127,46 @@ public class Pay  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+
+
         Intent intent = getIntent();
         PaymentKey = intent.getStringExtra(IntentKeys.PAYMENT_KEY);
-        IframeID = intent.getIntExtra(String.valueOf(IntentKeys.IFRAMEID),1);
+        IframeID = intent.getIntExtra(String.valueOf(IntentKeys.IFRAMEID), 1);
+        Endpoint = intent.getStringExtra(IntentKeys.ENDPOINT_URL);
 
-          StartPayment(PaymentKey,IframeID);
+        StartPayment(PaymentKey, IframeID);
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Boolean userCancelled;
+        if (id == R.id.close) {
+
+            userCancelled = true;
+
+            result.putExtra("userCancelled", userCancelled);
+            setResult(RESULT_OK,result);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            Runtime.getRuntime().gc();
+            finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/android_sdk_example/MainActivity.java
+++ app/src/main/java/com/example/android_sdk_example/MainActivity.java
@@ -1,26 +1,48 @@
 package com.example.android_sdk_example;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.androidsdk.IntentKeys;
 import com.example.androidsdk.Pay;
 
 public class MainActivity extends AppCompatActivity {
-    String paymentKey="ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SnZjbVJsY2w5cFpDSTZORFkxTWpVek5pd2liRzlqYTE5dmNtUmxjbDkzYUdWdVgzQmhhV1FpT21aaGJITmxMQ0ppYVd4c2FXNW5YMlJoZEdFaU9uc2labWx5YzNSZmJtRnRaU0k2SWtOc2FXWm1iM0prSWl3aWJHRnpkRjl1WVcxbElqb2lUbWxqYjJ4aGN5SXNJbk4wY21WbGRDSTZJa1YwYUdGdUlFeGhibVFpTENKaWRXbHNaR2x1WnlJNklqZ3dNamdpTENKbWJHOXZjaUk2SWpReUlpd2lZWEJoY25SdFpXNTBJam9pT0RBeklpd2lZMmwwZVNJNklrcGhjMnR2YkhOcmFXSjFjbWRvSWl3aWMzUmhkR1VpT2lKVmRHRm9JaXdpWTI5MWJuUnllU0k2SWtOU0lpd2laVzFoYVd3aU9pSmpiR0YxWkdWMGRHVXdPVUJsZUdFdVkyOXRJaXdpY0dodmJtVmZiblZ0WW1WeUlqb2lLemcyS0RncE9URXpOVEl4TURRNE55SXNJbkJ2YzNSaGJGOWpiMlJsSWpvaU1ERTRPVGdpTENKbGVIUnlZVjlrWlhOamNtbHdkR2x2YmlJNklrNUJJbjBzSW5CdGExOXBjQ0k2SWpFNU5pNHhOVE11TWpBdU1UWXhJaXdpWTNWeWNtVnVZM2tpT2lKSlRGTWlMQ0oxYzJWeVgybGtJam8wTnpBMUxDSmhiVzkxYm5SZlkyVnVkSE1pT2pFd01Dd2lhVzUwWldkeVlYUnBiMjVmYVdRaU9qRXpNekl3TENKbGVIQWlPakV3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREUxT0RNM05qQTBPRFo5LkVtbklnLWlWMFBRZFFtSkxYSFE5cFdYblNNb2swTFpTMEpJWUFWUm1pVGtobE5lVGxmLS05YmUyMEYtOVZyZmZpRUxhdFFqbXNEaVpYaUFYUXlHYmtR";
-    int IframeID= 21584;
 
+    String paymentKey = "ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SmpkWEp5Wlc1amVTSTZJa1ZIVUNJc0ltRnRiM1Z1ZEY5alpXNTBjeUk2TVRBd0xDSndiV3RmYVhBaU9pSTBNUzR5TXpRdU1qQTFMakl6T0NJc0ltVjRjQ0k2TVRVNU1UYzVNemN5Tnl3aWFXNTBaV2R5WVhScGIyNWZhV1FpT2pRNE5UZ3NJbXh2WTJ0ZmIzSmtaWEpmZDJobGJsOXdZV2xrSWpwbVlXeHpaU3dpZFhObGNsOXBaQ0k2TXpVMU5Td2liM0prWlhKZmFXUWlPalV5TURNMU5qTXNJbUpwYkd4cGJtZGZaR0YwWVNJNmV5Sm1hWEp6ZEY5dVlXMWxJam9pUTJ4cFptWnZjbVFpTENKc1lYTjBYMjVoYldVaU9pSk9hV052YkdGeklpd2ljM1J5WldWMElqb2lSWFJvWVc0Z1RHRnVaQ0lzSW1KMWFXeGthVzVuSWpvaU9EQXlPQ0lzSW1ac2IyOXlJam9pTkRJaUxDSmhjR0Z5ZEcxbGJuUWlPaUk0TURNaUxDSmphWFI1SWpvaVNtRnphMjlzYzJ0cFluVnlaMmdpTENKemRHRjBaU0k2SWxWMFlXZ2lMQ0pqYjNWdWRISjVJam9pUTFJaUxDSmxiV0ZwYkNJNkltTnNZWFZrWlhSMFpUQTVRR1Y0WVM1amIyMGlMQ0p3YUc5dVpWOXVkVzFpWlhJaU9pSXJPRFlvT0NrNU1UTTFNakV3TkRnM0lpd2ljRzl6ZEdGc1gyTnZaR1VpT2lJd01UZzVPQ0lzSW1WNGRISmhYMlJsYzJOeWFYQjBhVzl1SWpvaVRrRWlmWDAuMXdtdUQ3TUFfLVk0a19hWUpScnVNMm9ZWU9nZFJJWUxlWXFXRmV5RjdPYW5iQjEtZnFFdmZBTWxDbERNYVQyeFZPbkhGZnVYbkMtNUJjS1ZkVVJXaUE=";
+    int IframeID = 21734;
+    String Endpoint = "https://accept.paymobsolutions.com/api/acceptance/post_pay";
+    String success = "";
+    String Id = "";
+    String amount_cents = "";
+    Boolean userCancelled;
+    String integration_id = "";
+    String has_parent_transaction = "";
+    String txn_response_code = "";
+    String acq_response_code = "";
+    static String ErrorMessage;
+    private static final String TAG = "tag";
 
-    private void StartPayActivity(){
+    private void StartPayActivity() {
 
-      Intent pay_intent = new Intent(this, Pay.class);
-      pay_intent.putExtra(IntentKeys.PAYMENT_KEY, paymentKey);
-      pay_intent.putExtra(String.valueOf(IntentKeys.IFRAMEID), IframeID);
-      startActivity(pay_intent);
+        Intent pay_intent = new Intent(this, Pay.class);
+        pay_intent.putExtra(IntentKeys.PAYMENT_KEY, paymentKey);
+        pay_intent.putExtra(String.valueOf(IntentKeys.IFRAMEID), IframeID);
+        pay_intent.putExtra(IntentKeys.ENDPOINT_URL, Endpoint);
 
-  }
+        startActivityForResult(pay_intent, 1);
+
+    }
+
+    public static void setErrorMessage(String errorMessage) {
+
+        MainActivity.ErrorMessage = errorMessage;
+
+    }
 
 
     @Override
@@ -31,5 +53,39 @@ public class MainActivity extends AppCompatActivity {
         StartPayActivity();
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                success = data.getStringExtra("success");
+                Id = data.getStringExtra("ID");
+                amount_cents = data.getStringExtra("amount_cents");
+                Log.d(TAG, "amount cents: " + amount_cents);
+                integration_id = data.getStringExtra("integration_id");
+                Log.d(TAG, "integration_id: " + integration_id);
+                has_parent_transaction = data.getStringExtra("has_parent_transaction");
+                Log.d(TAG, "has_parent_transaction: " + has_parent_transaction);
+                txn_response_code = data.getStringExtra("txn_response_code");
+                Log.d(TAG, "txn_response_code: " + txn_response_code);
+
+
+                if (txn_response_code != null) {
+                    int ErrorCode = 5;
+                    if (ErrorCode == Integer.parseInt(txn_response_code)) {
+                        this.setErrorMessage("insufficient funds");
+
+                        Log.d(TAG, "Error Message:" + " " + ErrorMessage);
+                    }
+                }
+                acq_response_code = data.getStringExtra("acq_response_code");
+                Log.d(TAG, "acq_response_code: " + acq_response_code);
+                userCancelled = data.getBooleanExtra("userCancelled", false);
+                Log.d(TAG, "User Cancelled" + " " + userCancelled);
+            }
+        }
     }
 }
